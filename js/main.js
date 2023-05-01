9f51e090a6eb59b7af17ec87a715ac1646ad045a
Alexander-webov <vilmen37@gmail.com>
2023-05-01 09:14:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.services__content', {
+    slideClass: 'services__slider-box',
+    wrapperClass: 'services__slider',
+    autoplay: {
+        delay: 3000,
+    },
+    slidesPerView: 3,
+    spaceBetween: 20
+});
+
+const swiper2 = new Swiper('.project__content', {
+    slideClass: 'project__slider-box',
+    wrapperClass: 'project__slider',
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 6000,
+    },
+
+
+});
+
+
